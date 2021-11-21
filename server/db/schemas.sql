f604b94e70e72be4b9c9d305d964861a3fa20cfb
King Yiu Suen <kingyiusuen@gmail.com>
2021-11-20 19:48:50 -0600

    Use camelCase instead of underscore

--- server/db/schemas.sql
+++ server/db/schemas.sql
@@ -11,8 +11,8 @@ CREATE TABLE channels (
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users (id) NOT NULL,
-    channel_id INTEGER REFERENCES channels (id) NOT NULL,
+    userId INTEGER REFERENCES users (id) NOT NULL,
+    channelId INTEGER REFERENCES channels (id) NOT NULL,
     content TEXT NOT NULL,
-    created_at TIMESTAMPTZ
+    createdAt TIMESTAMPTZ
 );
